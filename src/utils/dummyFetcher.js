dfea9926d36128cc51554ad993370bfa8c4a342d
skchope711 <skchope711@gmail.com>
2022-06-04 10:02:07 +0900

    docs: dummyfetcher docs 추가

--- src/utils/dummyFetcher.js
+++ src/utils/dummyFetcher.js
@@ -6,6 +6,12 @@ const randomTimer = (func, args) => (resolve) => {
   }, getRandomSeconds());
 };
 
+/**
+ * fetch 함수기능을 모방한 dummyfetcher 함수
+ * @param {Function} func 내가받고 싶은 데이터를 반환하는 함수
+ * @param {Array} args 함수의 매개변수
+ * @returns
+ */
 const dummyFetcher = async (func, args) => {
   return new Promise(randomTimer(func, args));
 };
